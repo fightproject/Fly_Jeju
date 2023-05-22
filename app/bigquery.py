15c8c791de27f440bc71ad1cd91983e29bff6a80
leedain0301 <leedain0301@gmail.com>
2023-05-22 14:25:30 +0900

    ajax json 전송 에러 수정 중

--- app/bigquery.py
+++ app/bigquery.py
@@ -4,31 +4,25 @@ from google.oauth2 import service_account
 import os
 import pandas as pd
 
+def bigqueryCall(sql_input):
+        
+    # 서비스 계정 키 JSON 파일 경로
+    key_path = glob.glob("./app/config/*.json")[0]
 
+    # Credentials 객체 생성
+    credentials = service_account.Credentials.from_service_account_file(key_path)
+    
+    # GCP 클라이언트 객체 생성
+    client = bigquery.Client(credentials = credentials, 
+                            project = credentials.project_id)
 
-# 서비스 계정 키 JSON 파일 경로
-print(glob.glob("./config/*.json")[0])
-key_path = glob.glob("./config/*.json")[0]
+    # 데이터 조회 쿼리
+    sql = f'{sql_input}'
 
-# Credentials 객체 생성
-credentials = service_account.Credentials.from_service_account_file(key_path)
+    # 데이터 조회 쿼리 실행 결과
+    query_job = client.query(sql)
 
-# GCP 클라이언트 객체 생성
-client = bigquery.Client(credentials = credentials, 
-                         project = credentials.project_id)
-
-
-# 데이터 조회 쿼리
-sql = f"""
-SELECT
-FROM test_db.airplane    # 작성시 데이터셋이름.테이블명 으로 작성.
-limit 10
-"""
-
-# 데이터 조회 쿼리 실행 결과
-query_job = client.query(sql)
-
-
-# 데이터프레임 변환
-df = query_job.to_dataframe()
-print(df)
+    # 데이터프레임 변환
+    df = query_job.to_dataframe()
+    # print(df)
+    return df
--- app/main.py
+++ app/main.py
@@ -1,23 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from bigquery import *
+import json
 
 app = Flask(__name__, static_url_path="/static")
 
 
 @app.route('/')
 def calendar():
-    return render_template('main.html')
+
+    sql = '''
+    select date, day, price
+    from test_db.airplane 
+    limit 10
+    '''
+    data_list = bigqueryCall(sql)
+    print(data_list) #데이터 프레임
+
+    # 데이터 프레임을 JSON으로 변환
+    json_data = data_list.to_json(orient='records')
+    parsed_data = json.loads(json_data)
+    
+    print("데이터타입 : ", type(parsed_data))
+    for airplane in parsed_data:
+        print(airplane)
+    print("===================================== airplane 끝 =====================================")
+
+    return render_template('main.html',data_list=data_list, json_data=json_data)
 
 @app.route('/filght')
 def filght():
     return render_template('filght.html')
 
+
 @app.route('/dashboard1')
 def dashboard1():
     return render_template('dashboard1.html')
 
+
 @app.route('/dashboard2')
 def dashboard2():
     return render_template('dashboard2.html')
+
 
 if __name__ == '__main__':
     app.run(port='5000', debug=True)
